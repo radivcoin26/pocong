9e8032cfe6943632913361af072ef584d43e0bef
radivcoin26 <99596198+radivcoin26@users.noreply.github.com>
2022-04-02 00:59:20 +0700

    new windows

--- res/ley.sh
+++ res/ley.sh
@@ -1,21 +1,11 @@
 #bash
-firefox https://www.youtube.com/c/agungmujiono
-sleep 5
-firefox https://www.youtube.com/c/agungmujiono
-sleep 5
-firefox https://www.youtube.com/c/agungmujiono
-sleep 5
-firefox https://www.youtube.com/c/agungmujiono
-sleep 5
-firefox https://www.youtube.com/c/agungmujiono
-sleep 5
-firefox https://www.youtube.com/c/agungmujiono
-sleep 5
-firefox https://www.youtube.com/c/agungmujiono
-sleep 5
-firefox https://www.youtube.com/c/agungmujiono
-sleep 5
-firefox https://www.youtube.com/c/agungmujiono
-sleep 5
-firefox https://www.youtube.com/c/agungmujiono
-sleep 10
+firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA
+firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA
+firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA
+firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA
+firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA
+firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA
+firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA
+firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA
+firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA
+firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA
