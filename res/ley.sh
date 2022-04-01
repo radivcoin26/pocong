b15da98ff7f6b1d33cd24a5174b7c3e98d8b7efe
radivcoin26 <99596198+radivcoin26@users.noreply.github.com>
2022-04-02 01:00:50 +0700

    ?autoplay=1

--- res/ley.sh
+++ res/ley.sh
@@ -1,11 +1,11 @@
 #bash
-firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA
-firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA
-firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA
-firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA
-firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA
-firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA
-firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA
-firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA
-firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA
-firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA
+firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA?autoplay=1
+firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA?autoplay=1
+firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA?autoplay=1
+firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA?autoplay=1
+firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA?autoplay=1
+firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA?autoplay=1
+firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA?autoplay=1
+firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA?autoplay=1
+firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA?autoplay=1
+firefox --new-window https://www.youtube.com/watch?v=4LyHj-Es5_I&list=UUaATAGr88R_8a5SXa0Qm4hA?autoplay=1
