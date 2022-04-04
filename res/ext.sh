854249cfe07f508818809ac2645e484de890dc82
radivcoin26 <99596198+radivcoin26@users.noreply.github.com>
2022-04-05 04:17:39 +0700

    Update ext.sh

--- res/ext.sh
+++ res/ext.sh
@@ -1,12 +1,10 @@
-!yes '\n' | add-apt-repository ppa:saiarcot895/chromium-dev 
-!apt-get update
-!apt-get install chromium-browser -y
-!pip install pyppeteer2
-!pip install pyppeteer_stealth
-!pip install fake-useragent
-!wget https://raw.githubusercontent.com/radivcoin26/pocong/main/res/ley.sh 
-!chmod +x ley.sh
-!wget https://raw.githubusercontent.com/radivcoin26/pocong/main/res/iam.sh 
-!chmod +x iam.sh
-from IPython.display import clear_output
-clear_output()
+yes '\n' | add-apt-repository ppa:saiarcot895/chromium-dev 
+apt-get update
+apt-get install chromium-browser -y
+pip install pyppeteer2
+pip install pyppeteer_stealth
+pip install fake-useragent
+wget https://raw.githubusercontent.com/radivcoin26/pocong/main/res/ley.sh 
+chmod +x ley.sh
+wget https://raw.githubusercontent.com/radivcoin26/pocong/main/res/iam.sh 
+chmod +x iam.sh
