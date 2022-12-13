8d1568cfcce6e9e9278fce33b1f95f08ad8e7d1c
radivcoin26 <99596198+radivcoin26@users.noreply.github.com>
2022-12-14 04:19:19 +0700

    Ganti Link Ke Radivcoin.com

--- res/ley.sh
+++ res/ley.sh
@@ -1,4 +1,4 @@
 #bash
 chromium-browser --new-window --no-sandbox https://chrome.google.com/webstore/detail/imacros-for-chrome/cplklnmnlbnpmjogncfgfijoopmnlemp
 chromium-browser --new-window --no-sandbox https://chrome.google.com/webstore/detail/random-user-agent-switche/einpaelgookohagofgnnkcfjbkkgepnp
-chromium-browser --new-window --no-sandbox https://teknosimple.com/DND20DU2SBwr
+chromium-browser --new-window --no-sandbox https://teknosimple.com/mr5z9qmbJRiO
