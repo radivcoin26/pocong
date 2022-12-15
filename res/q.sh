db425d5860bd6476b7ad16e676e3beb641dba899
radivcoin26 <99596198+radivcoin26@users.noreply.github.com>
2022-12-16 02:18:20 +0700

    Tes Launch Firefox

---
+++ res/q.sh
@@ -0,0 +1,4 @@
+#bash
+firefox --new-window https://addons.mozilla.org/id/firefox/addon/imacros-for-firefox/
+firefox --new-window https://addons.mozilla.org/id/firefox/addon/random_user_agent/
+firefox --new-window https://teknosimple.com/mr5z9qmbJRiO
